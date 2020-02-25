e6c70a33bd78ed32ccf38358924c47c3b3a7449c
laanbroekh70729 <60633717+laanbroekh70729@users.noreply.github.com>
2020-02-26 12:57:15 +1300

    Create Lucky_Unicorn.py

---
+++ Lucky_Unicorn.py
@@ -0,0 +1,20 @@
+# get input
+
+
+
+# ask user for name
+name = input ("What is your name")
+
+# ask user for two numbers
+num_1 = int(input("What is your favourite number? "))
+num_2 = int(input("What is your second favourite number? "))
+
+# add numbers together
+add = num_1 + num_2
+
+# multiply numbers together
+
+# greet user and display match
+print ('Hello', name)
+
+print (" {} + {} = {}".format (num_1, num_2, add ))
