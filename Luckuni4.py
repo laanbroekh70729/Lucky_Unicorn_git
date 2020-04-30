639b9c391c82051df6705d406f053c48824a51cb
laanbroekh70729 <60633717+laanbroekh70729@users.noreply.github.com>
2020-04-30 23:34:38 +1200

    Create Luckuni4.py

---
+++ Luckuni4.py
@@ -0,0 +1,23 @@
+import random
+
+HOW_MUCH = 100
+tokens = ["horse", "zebra", "donkey", "unicorn"]
+
+unicorn_count = 0
+zebhor_count = 0
+donkey_count = 0
+
+for item in range(1,HOW_MUCH):
+
+    chosen = random.choice(tokens)
+
+    if chosen == "unicorn":
+        unicorn_count += 1
+    elif chosen == "donkey":
+        donkey_count += 1
+    else:
+        zebhor_count+= 1
+        
+        print(chosen)
+
+
