891e495765595d467ca8a280048a5290a4cfaa12
laanbroekh70729 <60633717+laanbroekh70729@users.noreply.github.com>
2020-06-11 11:25:09 +1200

    Update Luckyuni3.py

--- Luckyuni3.py
+++ Luckyuni3.py
@@ -1,26 +1,25 @@
  def int_check(question):
 
- error = "Please enter a number that is more than 0 and less than or equal to 10"
+    error = "Please enter a number that is more than 0 and less than or equal to 10"
 
- valid = False
+    valid = False
 
- while not valid:
-    try:
-        # Ask question
-        response = int(input(question))
+    while not valid:
+        try:
+            # Ask question
+            response = int(input(question))
 
-        if 0 < response <= 10:
+            if 0 < response <= 10:
+                return  response
 
-          return  response
+            # If response is invalid, display error
+            else:
+                print (error)
 
-          # If response is invalid, display error
-       else:
-           print (error)
-
-    except ValueError:
-     print(error)
+        except ValueError:
+            print(error)
 
 
-    amount = int_check("How much do you want to play with? ")
-    price = amount
-    print("You chose ${..2f}".format(price))
+amount = int_check("How much do you want to play with? ")
+price = amount
+print("You chose ${..2f}".format(price))
