b4314f3a74cd33c60eb6494893f50305155d3868
laanbroekh70729 <60633717+laanbroekh70729@users.noreply.github.com>
2020-06-11 11:21:11 +1200

    Create LU End game.py

---
+++ LU End game.py
@@ -0,0 +1,29 @@
+# Lucky unicorn End game
+
+# What this program should do
+# ask user how much they want to play with
+# if total less than $1, quit the program
+# if total more than $1, ask the user if they want to continue with the game
+
+
+
+# Assume starting amount will be $5
+total = 5
+
+# Allow manual token for testing
+token = input("enter a token")
+
+# Adjust total for a given token
+if token == "unicorn":
+    total += 5
+    feedback ="Congratulations you won $5.00"
+elif token=="Donkey":
+    total +- 0
+    feedback ="Apologies you did not win anything this round"
+else:
+    total += 2
+    feedback ="Congratulations you won $2.00 this round"
+
+
+
+
